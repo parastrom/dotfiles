aaaef54844dab9688a35bcdcc394e7d7afd00ba5
Tomisin <tomisin.adewole@cryppro.com>
2025-02-15 15:13:17 +0000

    changes

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -69,13 +69,13 @@ vim.opt.scrolloff = 8
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 vim.opt.guicursor = {
-  "n-v-c:block",                                  -- Normal, visual, command-line: block cursor
-  "i-ci-ve:ver25",                                -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
-  "r-cr:hor20",                                   -- Replace, command-line replace: horizontal bar cursor with 20% height
-  "o:hor50",                                      -- Operator-pending: horizontal bar cursor with 50% height
-  "a:blinkwait700-blinkoff400-blinkon250",        -- All modes: blinking settings
-  "sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
+	"n-v-c:block", -- Normal, visual, command-line: block cursor
+	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
+	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
+	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
+	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
 }
 
-vim.opt.spelllang = 'en_us'
+vim.opt.spelllang = "en_us"
 vim.opt.spell = true
